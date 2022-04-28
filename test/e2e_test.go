613abcdcf4c8199ec8ff5f0dd7507d796eda90f9
Hsn723 <me@atelierhsn.com>
2022-04-28 08:55:54 +0000

    CI and test improvements (#278)
    * update CI and fix flaky tests
    
    * enable dependabot updates for github actions

--- test/e2e_test.go
+++ test/e2e_test.go
@@ -10,6 +10,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -30,17 +31,16 @@ func getConfigPath() string {
 var _ = Describe("ct-monitor", func() {
 	kubectlOpts := k8s.NewKubectlOptions("kind-ct-monitor-kindtest", getConfigPath(), "default")
 
-	BeforeEach(func() {
-		k8s.KubectlApply(GinkgoT(), kubectlOpts, kubeResourcePath)
-	})
-	AfterEach(func() {
-		k8s.KubectlDelete(GinkgoT(), kubectlOpts, kubeResourcePath)
-	})
-
-	setCronjobStatus := func(suspend bool) {
-		status := strconv.FormatBool(suspend)
+	suspendCronjob := func() {
+		status := strconv.FormatBool(true)
 		By(fmt.Sprintf("setting cronjob suspend status to %s", status))
 		k8s.RunKubectl(GinkgoT(), kubectlOpts, "patch", "cronjobs", "ct-monitor", "-p", fmt.Sprintf("{\"spec\": {\"suspend\": %s }}", status))
+	}
+
+	triggerJob := func() {
+		By("triggering job from cronjob")
+		jobName := fmt.Sprintf("ct-monitor-%s", uuid.NewString())
+		k8s.RunKubectl(GinkgoT(), kubectlOpts, "create", "job", "--from=cronjob/ct-monitor", jobName)
 	}
 
 	checkLogOutput := func(label string, contains, notContains []string) {
@@ -54,30 +54,42 @@ var _ = Describe("ct-monitor", func() {
 		}
 	}
 
-	It("should run cronjob", func() {
-		By("listing pods")
+	BeforeEach(func() {
+		k8s.KubectlApply(GinkgoT(), kubectlOpts, kubeResourcePath)
+		suspendCronjob()
 		postfixListOpts := metav1.ListOptions{
 			LabelSelector: "app.kubernetes.io/name=postfix",
 		}
+		k8s.WaitUntilNumPodsCreated(GinkgoT(), kubectlOpts, postfixListOpts, 1, 5, 10*time.Second)
+		postfixPods := k8s.ListPods(GinkgoT(), kubectlOpts, postfixListOpts)
+		for _, pod := range postfixPods {
+			k8s.WaitUntilPodAvailable(GinkgoT(), kubectlOpts, pod.Name, 10, 10*time.Second)
+		}
+	})
+
+	AfterEach(func() {
+		k8s.KubectlDelete(GinkgoT(), kubectlOpts, kubeResourcePath)
+	})
+
+	It("should run cronjob", func() {
+		triggerJob()
+
+		By("listing pods")
 		ctMonitorListOpts := metav1.ListOptions{
 			FieldSelector: "status.phase==Succeeded",
 			LabelSelector: "app.kubernetes.io/name=ct-monitor",
 		}
-		k8s.WaitUntilNumPodsCreated(GinkgoT(), kubectlOpts, postfixListOpts, 1, 5, 10*time.Second)
-		k8s.WaitUntilNumPodsCreated(GinkgoT(), kubectlOpts, ctMonitorListOpts, 1, 10, 10*time.Second)
 
-		setCronjobStatus(true)
+		k8s.WaitUntilNumPodsCreated(GinkgoT(), kubectlOpts, ctMonitorListOpts, 1, 10, 10*time.Second)
 
 		By("ensuring email report has been sent")
 		checkLogOutput("app.kubernetes.io/name=ct-monitor", nil, []string{"error"})
 		checkLogOutput("app.kubernetes.io/name=postfix", []string{"from=", "removed"}, nil)
 
-		setCronjobStatus(false)
+		triggerJob()
 
 		By("checking for new pod")
 		k8s.WaitUntilNumPodsCreated(GinkgoT(), kubectlOpts, ctMonitorListOpts, 2, 10, 10*time.Second)
-
-		setCronjobStatus(true)
 
 		By("making sure positions file has been used")
 		checkLogOutput("app.kubernetes.io/name=ct-monitor", []string{"no new issuances observed"}, []string{"error"})
